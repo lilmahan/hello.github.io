fee19861aed4b0de2787471f32479b6f7ee18aa1
lilmahan <mahansabry343@gmail.com>
2025-04-26 22:08:42 +0330

    added files

--- trying/trying/settings.py
+++ trying/trying/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xqv$ndnld1hr$py)8^h-73(2gh7foghpyd9zi5055gf16f&c10'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
