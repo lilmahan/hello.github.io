d30a4310ac1123817c9a347da15f466966f53a1a
lilmahan <mahansabry343@gmail.com>
2025-05-02 19:07:38 +0330

    railway update

--- trying/trying/urls.py
+++ trying/trying/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from hello import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include("hello.urls"))
+    path('hello/', include("hello.urls")),
+    path('', views.index, name='index')
 ]
