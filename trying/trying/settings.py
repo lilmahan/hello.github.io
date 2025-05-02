8eb2ae4ee89c68c497b768684877c57c9bedc7c5
lilmahan <mahansabry343@gmail.com>
2025-05-02 19:39:19 +0330

    last update

--- trying/trying/settings.py
+++ trying/trying/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xqv$ndnld1hr$py)8^h-73(2gh7foghpyd9zi5055gf16f&c10
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['hellogithubio-production.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
